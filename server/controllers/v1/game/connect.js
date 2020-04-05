0e285aa280bb3733b6bf59ed519c8b6e1ef8dbf7
GuiMac <guibasconti@gmail.com>
2020-04-05 20:56:41 -0300

    Add active maps to cache

--- server/controllers/v1/game/connect.js
+++ server/controllers/v1/game/connect.js
@@ -16,21 +16,23 @@ const findDatabase = require('../../../utils/findDatabase');
 const { cachePaths, cacheTtls, tables, paths } = require('../../../utils/constants');
 
 module.exports = async (req, res, next) => {
+  const adventurerId = String(req.user.selectedAdventurer._id);
+  const mapId = String(req.user.selectedAdventurer.currentMap.id);
   let connectedPlayers = cache.get(cachePaths.CONNECTED_ADVENTURERS);
-  let mapData = cache.get(cachePaths.MAP_PREFIX + req.user.selectedAdventurer.currentMap.id);
+  let mapData = cache.get(cachePaths.MAP_PREFIX + mapId);
+  let activeMaps = cache.get(cachePaths.ACTIVE_MAPS);
   if (!connectedPlayers) {
     connectedPlayers = [];
   }
-  if (!connectedPlayers.includes(String(req.user.selectedAdventurer._id))) {
-    connectedPlayers.push(String(req.user.selectedAdventurer._id));
+  if (!connectedPlayers.includes(adventurerId)) {
+    connectedPlayers.push(adventurerId);
   }
   if (!mapData) {
-    const { id } = req.user.selectedAdventurer.currentMap;
     let map;
     try {
       map = await findDatabase(
         tables.MAPS,
-        { _id: id },
+        { _id: mapId },
         [],
         0,
         1
@@ -41,14 +43,19 @@ module.exports = async (req, res, next) => {
     mapData = require(`${paths.MAPS}${map.file}`);
     //  TODO: Spawn monsters
   }
-  if (!mapData.metadata.adventurers[req.user.selectedAdventurer._id]) {
-    mapData.metadata.adventurers[req.user.selectedAdventurer._id] = {
-      id: req.user.selectedAdventurer._id,
+  if (!mapData.metadata.adventurers[adventurerId]) {
+    mapData.metadata.adventurers[adventurerId] = {
       position: req.user.selectedAdventurer.currentMap.position
     };
   }
+  if (!activeMaps) {
+    activeMaps = [mapId];
+  } else if (!activeMaps.includes(mapId)) {
+    activeMaps.push(mapId);
+  }
   //  TODO: Check if we need to spawn monsters
   cache.set(cachePaths.CONNECTED_ADVENTURERS, connectedPlayers, cacheTtls.CONNECTED_ADVENTURERS);
-  cache.set(cachePaths.MAP_PREFIX + req.user.selectedAdventurer.currentMap.id, mapData, cacheTtls.MAP);
+  cache.set(cachePaths.MAP_PREFIX + mapId, mapData, cacheTtls.MAP);
+  cache.set(cachePaths.ACTIVE_MAPS, activeMaps, cacheTtls.ACTIVE_MAPS);
   return res.status(200).json();
 };
