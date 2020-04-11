0af0e8f6cef8a4fbe041ce2c1b16daced978b74c
GuiMac <guibasconti@gmail.com>
2020-04-11 16:36:32 -0300

    Add health mana and experience to send on adventurer metadata

--- server/game/utils/fillAdventurerInfoToAdventurerMapMetadata.js
+++ server/game/utils/fillAdventurerInfoToAdventurerMapMetadata.js
@@ -7,8 +7,11 @@ module.exports = (
   adventurerMapMetadata.adventurer = mapMetadata.adventurers[adventurerId];
   adventurerMapMetadata.adventurer = {
     position: adventurerMetadata.currentMap.position,
+    health: adventurerMetadata.health,
     currentHealth: adventurerMetadata.currentHealth,
+    mana: adventurerMetadata.mana,
     currentMana: adventurerMetadata.currentMana,
+    experience: adventurerMetadata.experience,
     target: adventurerMetadata.actions.target
   }
 };
