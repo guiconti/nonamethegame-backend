d2f734b07bd191e115a8e22145331a35ac7b96c3
GuiMac <guibasconti@gmail.com>
2020-04-18 22:48:05 -0300

    Calculate hit and miss probabilities

--- server/game/entityAttackEntity.js
+++ server/game/entityAttackEntity.js
@@ -4,24 +4,34 @@ const { game, sockets } = require('../constants');
 
 module.exports = (attacker, defender, sendMessageId = null) => {
   //  For now damage calculation is attack - defense
-  const maxAttack = attacker.attack;
-  const minAttack = Math.floor(
-    maxAttack * (1 - game.DAMAGE_PERCENTAGE_VARIATION)
-  );
-  const finalAttack =
-    Math.floor(Math.random() * (maxAttack - minAttack + 1)) + minAttack;
-  const damage = Math.max(finalAttack - defender.defense, 1);
-  defender.currentHealth -= damage;
-  if (defender.currentHealth <= 0) {
-    defender.dead = true;
-  }
-  if (sendMessageId) {
-    let message = '';
+  let message = '';
+  //  Try to hit
+  const chanceToHit = attacker.hit / defender.flee;
+  if (Math.random() > chanceToHit) { // Miss
+    if (attacker._id === sendMessageId) {
+      message = `<div>You missed the attack on ${defender.name}.</div>`;
+    } else {
+      message = `<div>${attacker.name} missed the attack on you.</div>`;
+    }
+  } else { // Hit
+    const maxAttack = attacker.attack;
+    const minAttack = Math.floor(
+      maxAttack * (1 - game.DAMAGE_PERCENTAGE_VARIATION)
+    );
+    const finalAttack =
+      Math.floor(Math.random() * (maxAttack - minAttack + 1)) + minAttack;
+    const damage = Math.max(finalAttack - defender.defense, 1);
+    defender.currentHealth -= damage;
+    if (defender.currentHealth <= 0) {
+      defender.dead = true;
+    }
     if (attacker._id === sendMessageId) {
       message = `<div>Dealt <span class="bold">${damage}</span> to ${defender.name}</div>`;
     } else {
       message = `<div>Received <span class="bold">${damage}</span> from ${attacker.name}</div>`;
     }
+  }
+  if (sendMessageId) {
     webSocket.emit(sendMessageId, sockets.BATTLE_LOG_MESSAGE, message);
   }
   addAttackCooldown(attacker);
